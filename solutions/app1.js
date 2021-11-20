b36d5b2f374a9fe4ff54cd7c0220708cd65919b7
Joe <jp.szonyegi@gmail.com>
2021-11-20 13:15:32 +0100

    Solved

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,7 +1,17 @@
-
+const converterPromise = (arr) => {
+    return new Promise((resolve, reject) => {
+        if (arr.every(item => typeof item === 'string')) {
+            arr = arr.map(x => x.toLocaleUpperCase());
+            resolve(arr);        
+        } else {
+            reject('Error: Not all elements are string type!')
+        }
+    })
+}
 
 
 
 
 
 // export default converterPromise;
+export default converterPromise;
--- solutions/app2.js
+++ solutions/app2.js
@@ -1,7 +1,14 @@
-
-
-
-
+const sorterPromise = (arr) => {
+    return new Promise((resolve, reject) => {
+        if (arr.every(item => typeof item === 'string')) {
+            arr = arr.sort();
+            resolve(arr);
+        } else {
+            reject('Error: Not all elements are string type!');
+        }
+    })
+}
 
 
 // export default sorterPromise;
+export default sorterPromise;
